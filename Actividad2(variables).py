66ca58fc0e1203fb4bb9af86ba5f17c8540535bd
Byron Chico <e22101@ism.edu.ec>
2026-04-15 09:08:48 -0500

    Actualizacion2

---
+++ Actividad2(variables).py
@@ -0,0 +1,26 @@
+
+# %%
+# Escribe un comentario de múltiples líneas con: nombre, fecha y título.
+"""Nombre = Byron
+Fecha = 15/4/26
+ Titulo = Actividad2"""
+# %%
+nombre= "Byron"
+apellido= "Chico"
+nombreCompleto= "Byron Chico"
+pais= "Ecuador"
+ciudad= "Calderon"
+edad= "16"
+anio= "2026"
+estaCasado= "No"
+esVerdadero= "Si"
+luzEncendida= "Si"
+hobby,deporte,cumpleanos= "Leer","Futbol","31 de Diciembre 2009"
+# %%
+nombre= "Byron"
+apellido= "Chico"
+nombreCompleto= "Byron Chico"
+print(type(nombre))
+print(type(apellido))
+print(type(nombreCompleto))
+# %%
