c87f873e442fba7d80c3a743d8b1ce062bdc8fce
Byron Chico <e22101@ism.edu.ec>
2026-04-21 10:57:14 -0500

    Actividad 4

---
+++ Actividad4 (strings).py
@@ -0,0 +1,20 @@
+StringVariaslineas= """
+Byron
+Informatica ruta 3
+"""
+print(StringVariaslineas)
+Colegio= "Ism"
+longitud= len(Colegio)
+print(longitud)
+print(len)
+print(len("San francisco de quito"))
+Nombre= "Byron"
+Apellido= "Chico"
+Nombre_completo= Nombre+ "" +Apellido+ ""
+print(Nombre_completo)
+print("Mi nombre completo es",Nombre,Apellido)
+print(Nombre_completo*5)
+print("python\nchallenge")
+print("Nombre\tsemana1\tsemana2\tsemana3")
+print("simbolo(\\)")
+print(f"Mi nombre es:{Nombre} y mi Apellido es {Apellido}")
