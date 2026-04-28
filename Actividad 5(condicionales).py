7c92fb5b93a29dfa33b7bfd0e7efad8db5329e4b
Byron Chico <e22101@ism.edu.ec>
2026-04-28 12:32:56 -0500

    Actividad 5.2

--- Actividad 5(condicionales).py
+++ Actividad 5(condicionales).py
@@ -18,3 +18,37 @@ else:
         else:
             if nota < 70:
                 print("D")
+# %%
+# Ejercicio de if anillados
+numero= float(input("Ingrese un numero"))
+if numero== 0:
+    print("El numero es 0")
+else:
+    if numero>0:
+        if (numero % 2==0):
+            print("El numero es par y positivo")
+        else:
+            print("El numero es impar y positivo")
+    else:
+        if (numero % 2==0):
+            print("El numero es par y negavitivo")
+        else:
+            print("El numero es impar y negativo")
+# %%
+numero= float(input("Ingrese un numero"))
+if numero > 0 and numero % 2==0:
+    print("El numero es par y positivo")
+elif numero > 0 and numero % 2==1:
+    print("El numero es impar y positivo")
+elif numero < 0 and numero % 2==0:
+    print("El numero es par y negativo")
+elif numero < 0 and numero % 2==1:
+    print("El numero es impar y negavito")
+else:
+    print("El numero es 0")
+# %%
+# Short hand
+numero= float(input("Ingrese un numero"))
+print("Numero es positivo") if numero > 0 else print ("Numero es negativo")
+print("Numero es par") if numero % 2 ==0 else print ("Numero es impar")
+print("Numero es 0") if numero == 0 else print ("Gracias por usar")
