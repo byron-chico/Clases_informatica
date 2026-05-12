f4c7e1f705f9cc0c23ab242f83cac7da2f239358
Byron Chico <e22101@ism.edu.ec>
2026-05-12 12:38:06 -0500

    yeyeye

--- Actividad6 (cido while).py
+++ Actividad6 (cido while).py
@@ -15,3 +15,29 @@ while opcion != "C":
         print("Saliendo del programa")
     else:
         print("Opción no válida")
+#%%
+numbers= [0,1,2,3,4,5]
+for number in numbers:
+    print(number)
+#%%
+Notas = [8,7,9,10,6]
+suma = 0
+for nota in Notas:
+    suma= suma + nota
+promedio = suma / len(Notas)
+print(promedio)
+#%%
+language = "Python"
+for letter in language:
+    print(letter)
+#%%
+palabra= input("Ingrese una palabra: ")
+vocales = 0
+consonantes = 0
+for letra in palabra:
+    if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+            vocales= vocales + 1
+    else:
+        consonantes += 1
+
+print(f"La palabra '{palabra}' tiene {vocales} vocales y {consonantes} consonantes, tiene {consonantes + vocales} caracteres en total")
